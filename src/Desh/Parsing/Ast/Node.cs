a58bdb95f17319ab73cce13b463ef447afca0935
Ivan Akcheurov <ivan.akcheurov@gmail.com>
2019-02-10 12:05:32 +0100

    Do not serialize full desh with every AST node NO_BUILD

--- src/Desh/Parsing/Ast/Node.cs
+++ src/Desh/Parsing/Ast/Node.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using YamlDotNet.Serialization;
 
 namespace Desh.Parsing.Ast
 {
     public abstract class Node
     {
+        [YamlIgnore]
         public string SourceDesh { get; set; }
         public string SourceDeshLocation { get; set; }
 
