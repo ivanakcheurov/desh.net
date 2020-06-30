8c2367f7f8e2ad994a9686b00172a8d213aa095d
Ruslan Hamzatov <ruslan.hamzatov@albelli.com>
2020-06-30 13:29:25 +0200

    Upgrade .NET version (#11)
    * upgrade dot net version to latest LTS
    
    * upgrade C# version usage
    
    * remove unused var
    
    * upgrade C# version usage
    
    * update build env

--- src/Desh/Execution/Engine.cs
+++ src/Desh/Execution/Engine.cs
@@ -13,15 +13,13 @@ namespace Desh.Execution
         private readonly IVariableEvaluator _variableEvaluator;
         private readonly IOperatorEvaluator _operatorEvaluator;
         private readonly IExecutionLogger _executionLogger;
-        private bool _stopOnFirstAcceptedDecision;
         private int _currentExecutionLogStepNumber = 1;
 
-        public Engine(IVariableEvaluator variableEvaluator, IOperatorEvaluator operatorEvaluator, IExecutionLogger executionLogger, bool stopOnFirstAcceptedDecision)
+        public Engine(IVariableEvaluator variableEvaluator, IOperatorEvaluator operatorEvaluator, IExecutionLogger executionLogger)
         {
             _variableEvaluator = variableEvaluator;
             _operatorEvaluator = operatorEvaluator;
             _executionLogger = executionLogger;
-            _stopOnFirstAcceptedDecision = stopOnFirstAcceptedDecision;
         }
 
         public async Task<EvaluationResult> Execute(ExpressionBlock expressionBlock)
@@ -139,14 +137,13 @@ namespace Desh.Execution
         {
             foreach (var @operator in operatorAndMapping.Operators)
             {
-                
                 var match = EvaluateOperator(variableValue, @operator.Name, @operator.Arguments, @operator.SourceDeshLocation);
                 if (match == false)
                 {
                     return LogReturn(null);
                 }
             }
-            
+
             // todo: consider allowing ThenExpressionBlock and Decision be specified at the same time
             //
             if (operatorAndMapping.ThenExpressionBlock != null)
@@ -179,20 +176,13 @@ namespace Desh.Execution
                 Type = StepType.ReturnExpressionResult,
             };
 
-            switch (evaluationResult)
+            step.Result = evaluationResult switch
             {
-                case Conclusion conclusion:
-                    step.Result = conclusion.Decisions.Single();
-                    break;
-                case PositiveEval _:
-                    step.Result = nameof(PositiveEval);
-                    break;
-                case null:
-                    step.Result = "<null>";
-                    break;
-                default:
-                    throw new InvalidOperationException("Unexpected evaluation result: " + evaluationResult);
-            }
+                Conclusion conclusion => conclusion.Decisions.Single(),
+                PositiveEval _ => nameof(PositiveEval),
+                null => "<null>",
+                _ => throw new InvalidOperationException("Unexpected evaluation result: " + evaluationResult)
+            };
             _executionLogger.AddStep(step);
             return evaluationResult;
         }
